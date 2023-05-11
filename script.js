13e4c1078320b325a21528b81440b420bd2fd8fe
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-11 18:47:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function threeSum(arr, target) {
 // write your code here
-  for(let i = 0; i<arr.length; i++){
+  
+	
+	
+	/*
+	for(let i = 0; i<arr.length; i++){
 	  let rem = arr[i]%2;
 	  let dec = arr[i]/2;
 	  target.push(rem);
   }
-	return target.reverse();
+	return target.reverse();*/
 }
+
+let dec = parseInt(prompt("Enter no:"));
+let binary = "";
+while(dec>0){
+	binary = (dec%2) + binary;
+	dec = Math.floor(dec/2);
+}
+console.log(binary);
 
 module.exports = threeSum;
